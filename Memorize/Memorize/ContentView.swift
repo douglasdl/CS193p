c1f4a42ba7127ee5710dbb6c37201e7827a1f639
Douglas <douglas_san@hotmail.com>
2021-03-19 07:14:17 -0300

    Add files via upload

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Douglas Dias Leal on 19/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
